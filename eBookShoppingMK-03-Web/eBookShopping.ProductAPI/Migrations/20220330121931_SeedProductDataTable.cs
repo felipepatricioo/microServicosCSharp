e0e1fc0bad71a909d5f0512bc797fcca876674fc
Felipe Patrício <86684357+felipepatricioo@users.noreply.github.com>
2022-03-31 10:21:44 -0300

    aaaa

---
+++ eBookShoppingMK-03-Web/eBookShopping.ProductAPI/Migrations/20220330121931_SeedProductDataTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eBookShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 3L, "Romance", "", "", "Name", 69.9m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+        }
+    }
+}
