5dc8fd65dc7f6285affc8db12424e9e86d294fac
Felipe Patrício <felioppatricio@gmail.com>
2022-04-01 09:09:37 -0300

    teste

--- eBookShoppingMK-02-Architecture/eBookShopping.ProductAPI/Models/Context/MySQLContext.cs
+++ eBookShoppingMK-02-Architecture/eBookShopping.ProductAPI/Models/Context/MySQLContext.cs
@@ -12,5 +12,20 @@ namespace eBookShopping.ProductAPI.Models.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "",
+                CategoryName = "Romance",
+                ImageUrl = ""
+
+            });
+        }
+
     }
 }
