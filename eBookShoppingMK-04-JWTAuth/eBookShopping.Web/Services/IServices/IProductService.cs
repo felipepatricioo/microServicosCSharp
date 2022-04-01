19047b8363f91dd0289ed529b9f4c6c1abb17751
Felipe Patrício <felioppatricio@gmail.com>
2022-04-01 11:40:48 -0300

    adding the authentication folder

---
+++ eBookShoppingMK-04-JWTAuth/eBookShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using eBookShopping.Web.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eBookShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+
+        Task<ProductModel> FindProductById(long id);
+
+        Task<ProductModel> Create(ProductModel model);
+
+        Task<ProductModel> Update(ProductModel model);
+
+        Task<bool> DeleteProductById(long id);
+
+    }
+}
