e7d642c31e8b3320ba6ada0a288f9cde16f21191
Felipe Patrício <felioppatricio@gmail.com>
2022-04-27 15:58:28 -0300

    updating some thingsssssssssssss many errors tho fuck my life bruh

--- eBookShoppingMk-05-ShoppingCart/eBookShopping.CartAPI/Models/Context/MySQLContext.cs
+++ eBookShoppingMk-05-ShoppingCart/eBookShopping.CartAPI/Models/Context/MySQLContext.cs
@@ -11,7 +11,7 @@ namespace eBookShopping.CartAPI.Models.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
-        public DbSet<CartDetail> CartDetail { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
         public DbSet<CartHeader> CartHeaders { get; set; }
 
 
