3782153417595b96c904c8dbea747bd6488bcad3
Felipe Patrício <felioppatricio@gmail.com>
2022-06-08 15:57:48 -0300

    xzzvxvxcv

---
+++ eBookShoppingMk-05-ShoppingCart/eBookShopping.Email/Models/EmailLog.cs
@@ -0,0 +1,20 @@
+﻿using eBookShopping.Email.Models.Base;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eBookShopping.Email.Models
+{
+    [Table("email_logs")]
+    public class EmailLog : BaseEntity
+    {
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("log")]
+        public string Log { get; set; }
+
+        [Column("sent_date")]
+        public DateTime SentDate { get; set; }
+    }
+}
---
+++ eBookShoppingMk-05-ShoppingCart/eBookShopping.Email/Repository/IEmailRepository.cs
@@ -0,0 +1,10 @@
+﻿using eBookShopping.Email.Messages;
+using System.Threading.Tasks;
+
+namespace eBookShopping.Email.Repository
+{
+    public interface IEmailRepository
+    {
+        Task LogEmail(UpdatePaymentResultMessage message);
+    }
+}
